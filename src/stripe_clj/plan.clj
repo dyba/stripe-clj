361aaa4fd4ab7bef055944a51f1182d6ac2af59e
Daniel Dyba <daniel.dyba@gmail.com>
2014-08-09 22:11:40 -0700

    Add plan and subscription models

---
+++ src/stripe_clj/plan.clj
@@ -0,0 +1,32 @@
+(ns stripe-clj.plan)
+
+(defn create
+  [params & optns]
+  {}
+  
+  )
+
+(defn retrieve
+  [params & optns]
+  {}
+  
+  )
+
+(defn update
+  [params & optns]
+  {}
+  
+  )
+
+(defn delete
+  [params & optns]
+  {}
+  
+  )
+
+(defn list-all
+  [params & optns]
+  {}
+  
+  )
+
---
+++ src/stripe_clj/subscription.clj
@@ -0,0 +1,30 @@
+(ns stripe-clj.subscription)
+
+(defn create
+  [params & opts]
+  
+  )
+
+(defn retrieve
+  [params & opts]
+  )
+
+(defn update
+  [params & opts]
+  
+  )
+
+(defn delete
+  [params & opts]
+  
+  )
+
+(defn cancel
+  [params & opts]
+  
+  )
+
+(defn list-all
+  [params & opts]
+  
+  )
